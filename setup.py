a8d0d4b665f0507fcc77f7af0b3a094c32411b9a
srw <srw@nmd.so>
2016-09-25 14:35:46 -0700

    Change Name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name = 'django-private-media',
+    name = 'django-private-fork',
     version = '0.1.5',
     packages = find_packages(),
     include_package_data = True,
